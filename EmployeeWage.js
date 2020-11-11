811785bcff4388e6addb54df53b7a272e09c3754
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-11 21:22:19 +0530

    Added:To check employee is present or not

---
+++ EmployeeWage.js
@@ -0,0 +1,7 @@
+console.log("Welcome to EE Wage Computation Program")
+var isPresent=1
+var randomCheck=Math.floor(Math.random() * 10)%2
+if (isPresent == randomCheck)
+    console.log ("Employee is Present")
+else
+    console.log ("Employee is not Present")
