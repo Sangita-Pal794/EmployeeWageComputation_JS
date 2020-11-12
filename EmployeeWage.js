dc14375061765f295c7f7572ba6f59f518311b16
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-12 10:35:30 +0530

    Added:Function To calculate total monthly wages

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,13 +3,14 @@ const EE_WAGE_DETAILS = {
   WAGE_PER_HOUR: 20,
   FULL_TIME_HOURS: 8,
   PART_TIME_HOURS: 4,
+  DAYS_PER_MONTH: 20,
 };
 Object.freeze = EE_WAGE_DETAILS;
 
-let getRandom = Math.floor(Math.random() * 3);
+let getRandom = () => Math.floor(Math.random() * 3);
 
 let getWorkHours = () => {
-    switch (getRandom) {
+    switch (getRandom()) {
       case 1:
         return EE_WAGE_DETAILS.FULL_TIME_HOURS;
       case 2:
@@ -18,5 +19,14 @@ let getWorkHours = () => {
         return 0;
     }
 };
-let computeEEWage = EE_WAGE_DETAILS.WAGE_PER_HOUR * getWorkHours();
-console.log(computeEEWage);
+let computeEEWage = () => {
+    let totalWage = 0,
+      dayCount = 1;
+    while (dayCount <= EE_WAGE_DETAILS.DAYS_PER_MONTH) {
+        totalWage += EE_WAGE_DETAILS.WAGE_PER_HOUR * getWorkHours();
+        dayCount++;
+    }
+    return totalWage;
+};
+
+console.log(computeEEWage());
