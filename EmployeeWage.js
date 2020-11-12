10a523ccb16fe1a57e19904f7a153324c60ef779
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-12 10:21:04 +0530

    Refactored:Code using switch case

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,4 @@
+console.log("Welcome to EE Wage Computation Program");
 const EE_WAGE_DETAILS = {
   WAGE_PER_HOUR: 20,
   FULL_TIME_HOURS: 8,
@@ -7,13 +8,15 @@ Object.freeze = EE_WAGE_DETAILS;
 
 let getRandom = Math.floor(Math.random() * 3);
 
-let getWorkHours = () =>
-  getRandom === 1
-    ? EE_WAGE_DETAILS.FULL_TIME_HOURS
-    : getRandom === 2
-    ? EE_WAGE_DETAILS.PART_TIME_HOURS
-    : 0;
-
+let getWorkHours = () => {
+    switch (getRandom) {
+      case 1:
+        return EE_WAGE_DETAILS.FULL_TIME_HOURS;
+      case 2:
+        return EE_WAGE_DETAILS.PART_TIME_HOURS;
+      default:
+        return 0;
+    }
+};
 let computeEEWage = EE_WAGE_DETAILS.WAGE_PER_HOUR * getWorkHours();
-
 console.log(computeEEWage);
