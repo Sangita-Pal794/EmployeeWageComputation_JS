8d526991c0193ef892eef52512fd75b44fb390f8
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-12 10:06:56 +0530

    Refactored: Optimized Code and reduced the lines & add part time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,12 +1,20 @@
 console.log("Welcome to EE Wage Computation Program");
-let getRandom = Math.floor(Math.random() * 2);
-let isEEPresent = () => getRandom === 1;
-        const EE_WAGE_DETAILS = {
-        wagePerHour: 20,
-        hoursPerDay: 8,
-      };
-      Object.freeze = EE_WAGE_DETAILS;
-      
-      let computeEEWage = () =>
-        isEEPresent() ? EE_WAGE_DETAILS.wagePerHour * EE_WAGE_DETAILS.hoursPerDay : 0;
-        //console.log("End Of EE Wage Computation Program")
+const EE_WAGE_DETAILS = {
+  WAGE_PER_HOUR: 20,
+  FULL_TIME_HOURS: 8,
+  PART_TIME_HOURS: 4,
+};
+Object.freeze = EE_WAGE_DETAILS;
+
+let getRandom = Math.floor(Math.random() * 3);
+
+let getWorkHours = () =>
+  getRandom === 1
+    ? EE_WAGE_DETAILS.FULL_TIME_HOURS
+    : getRandom === 2
+    ? EE_WAGE_DETAILS.PART_TIME_HOURS
+    : 0;
+
+let computeEEWage = EE_WAGE_DETAILS.WAGE_PER_HOUR * getWorkHours();
+
+console.log(computeEEWage);
